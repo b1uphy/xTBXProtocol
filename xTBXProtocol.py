189f5f01fc0740bdb00de12dcda20f53fee6c592
bluphy <bluphyxw@gmail.com>
2018-06-07 18:40:05 +0800

    Init

---
+++ xTBXProtocol.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+'''
+contact:
+bluphy@qq.com
+@author: bluphy
+Created on 2018-06-07 18:38 by xw
+This is a tbox internal communication protocol library.
+'''
